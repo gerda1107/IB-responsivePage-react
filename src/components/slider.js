40842468824bc6a27fd6b2cd7459b14a837900b1
gerda1107 <gerda.baltrusaityte@gmail.com>
2021-05-22 16:58:21 +0300

    added and tested slider.js component and functionality

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,7 +1,36 @@
+import { SliderData } from "./sliderData";
+import React, { useState } from 'react';
+
 function Slider() {
+
+    const [getCurrentSlide, setCurrentSlide] = useState(0);
+    const [getDotActiveStatus, setDotActiveStatus] = useState(0);
+
+    const changeSlide = (n) => {
+        setCurrentSlide(n);
+        setDotActiveStatus(n);
+    }
+
     return (
-        <div>
-            hello
+        <div className="slider-container">
+            {SliderData.map((slide, index) => {
+                return (
+                    <div className="slider-img" key={index}>
+                        <img src={slide.image} alt="products"
+                            style={index === getCurrentSlide ? { display: "block" } : { display: "none" }}>
+                        </img>
+                    </div>  
+                )
+            })}
+
+            <div className="dot-container">
+                {Array.from(Array(3), (x, i) => {
+                    return <span
+                        className={i === getDotActiveStatus ? "dot active" : "dot"}
+                        onClick={() => changeSlide(i)}
+                        key={i}></span>
+                })}
+            </div>
         </div>
     )
 }
